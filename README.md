64c1c46af5cdd2788b15b022c9bc874f9a2c6aa5
bilal simsek <xyzbilal@gmail.com>
2023-07-13 12:41:07 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tape_measure
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tape_measure/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tape_measure
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
